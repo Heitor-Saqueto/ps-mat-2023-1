b777e349f90afa1130e7afe82fcd91cb533b1a5d
Heitor Saqueto <heitorsaqueto@hotmail.com>
2023-05-30 10:29:37 -0300

    Testando

---
+++ front-end/src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import HeaderBar from './components/ui/HeaderBar'
+import Box from '@mui/material/Box'
+import PaymentMethodList from './pages/payment_method/PaymentMethodList'
+import PaymentMethodForm from './pages/payment_method/PaymentMethodForm'
+
+function AuthGuard({children}) {
+  // Estaremos autenticados se tivermos um token gravado no localStorage
+  if(window.localStorage.getItem('token')) return children
+  else return <Navigate to="/login" replace />
+}
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <HeaderBar />
+      <Box sx={{ m: '25px auto', p: '25px' }}>
+        <Routes>
+          
+          <Route path="/login" element={<Login />} />
+          
+          <Route path="/" element={ 
+            <AuthGuard> <Home /> </AuthGuard> 
+          } />
+          
+          <Route path="/payment_method" element={ 
+            <AuthGuard> <PaymentMethodList /> </AuthGuard> 
+          } />
+
+          <Route path="/payment_method/new" element={ 
+            <AuthGuard> <PaymentMethodForm /> </AuthGuard> 
+          } />
+
+          <Route path="/payment_method/:id" element={ 
+            <AuthGuard> <PaymentMethodForm /> </AuthGuard> 
+          } />
+
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ front-end/src/pages/payment_method/PaymentMethodForm.jsx
@@ -0,0 +1,196 @@
+import React from 'react'
+import PageTitle from '../../components/ui/PageTitle'
+import TextField from '@mui/material/TextField'
+import SendIcon from '@mui/icons-material/Send'
+import Fab from '@mui/material/Fab'
+import myfetch from '../../utils/myfetch'
+import Backdrop from '@mui/material/Backdrop'
+import CircularProgress from '@mui/material/CircularProgress'
+import Notification from '../../components/ui/Notification'
+import { useNavigate, useParams } from 'react-router-dom'
+import PaymentMethod from '../../models/PaymentMethod'
+import getValidationMessages from '../../utils/getValidationMessages'
+
+export default function PaymentMethodForm() {
+  const API_PATH = '/payment_methods'
+
+  const navigate = useNavigate()
+  const params = useParams()
+
+  const [state, setState] = React.useState({
+    paymentMethod: {
+      description: '',
+      operator_fee: ''
+    },
+    errors: {},
+    showWaiting: false,
+    notif: {
+      show: false,
+      severity: 'success',
+      message: ''
+    }
+  })
+  const {
+    paymentMethod,
+    errors,
+    showWaiting,
+    notif
+  } = state
+
+  function handleFormFieldChange(event) {
+    const paymentMethodCopy = {...paymentMethod}
+    paymentMethodCopy[event.target.name] = event.target.value
+    setState({...state, paymentMethod: paymentMethodCopy})
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault()    // Evita que a página seja recarregada
+    // Envia os dados para o back-end
+    sendData()
+  }
+
+  // Este useEffect será executando apenas durante o carregamento
+  // inicial da página
+  React.useEffect(() => {
+    // Se houver parâmetro id na rota, devemos carregar um registro
+    // existente para edição
+    if(params.id) fetchData()
+  }, [])
+
+  async function fetchData() {
+    setState({...state, showWaiting: true, errors:{}})
+    try {
+      const result = await myfetch.get(`${API_PATH}/${params.id}`)
+      setState({
+        ...state,
+        paymentMethod: result,
+        showWaiting: false
+      })
+    }
+    catch(error) {
+      console.error(error)
+      setState({
+        ...state, 
+        showWaiting: false,
+        errors: errorMessages,
+        notif: {
+          severity: 'error',
+          show: true,
+          message: 'ERRO: ' + error.message
+        }
+      })
+    }
+  }
+
+  async function sendData() {
+    setState({...state, showWaiting: true, errors: {}})
+    try {
+      
+      // Chama a validação da biblioteca Joi
+      await PaymentMethod.validateAsync(paymentMethod, { abortEarly: false })
+
+      // Registro já existe: chama PUT para atualizar
+      if (params.id) await myfetch.put(`${API_PATH}/${params.id}`, paymentMethod)
+      
+      // Registro não existe: chama POST para criar
+      else await myfetch.post(API_PATH, paymentMethod)
+
+      setState({
+        ...state, 
+        showWaiting: false,
+        notif: {
+          severity: 'success',
+          show: true,
+          message: 'Item salvo com sucesso'
+        }
+      })
+    }
+    catch(error) {
+      const { validationError, errorMessages } = getValidationMessages(error)
+
+      console.error(error)
+      
+      setState({
+        ...state, 
+        showWaiting: false,
+        errors: errorMessages,
+        notif: {
+          severity: 'error',
+          show: !validationError,
+          message: 'ERRO: ' + error.message
+        }
+      })
+    }
+  }
+
+  function handleNotifClose(event, reason) {
+    if (reason === 'clickaway') {
+      return;
+    }
+    
+    // Se o item foi salvo com sucesso, retorna à página de listagem
+    if(notif.severity === 'success') navigate(-1)
+
+    setState({ ...state, notif: { ...notif, show: false } })
+  }
+
+  return (
+    <>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={showWaiting}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+
+      <Notification 
+        show={notif.show}
+        severity={notif.severity}
+        onClose={handleNotifClose}
+      >
+        {notif.message}
+      </Notification>
+      
+      <PageTitle 
+        title={params.id ? "Editar método de pagamento" : "Cadastrar novo método de pagamento"} 
+      />
+
+      <form onSubmit={handleFormSubmit}>
+        <TextField 
+          label="Descrição" 
+          variant="filled"
+          fullWidth
+          required
+          name="description"  // Nome do campo na tabela
+          value={paymentMethod.description}   // Nome do campo na tabela
+          onChange={handleFormFieldChange}
+          error={errors?.description}
+          helperText={errors?.description}
+        />
+
+        <TextField 
+          label="Taxa de operação" 
+          variant="filled"
+          type="number"
+          fullWidth
+          required
+          name="operator_fee"  // Nome do campo na tabela
+          value={paymentMethod.operator_fee}   // Nome do campo na tabela
+          onChange={handleFormFieldChange}
+          error={errors?.operator_fee}
+          helperText={errors?.operator_fee}
+        />
+
+        <Fab 
+          variant="extended" 
+          color="secondary"
+          type="submit"
+        >
+          <SendIcon sx={{ mr: 1 }} />
+          Enviar
+        </Fab>
+
+      </form>
+    </>
+  )
+}
